8309c60e09d2176b5e15c88fe7d6b3f4151d58f0
Felix Mumme <felix.mumme@gmx.net>
2020-05-29 23:16:16 +0200

    first working version

--- src/BasicRouter.tsx
+++ src/BasicRouter.tsx
@@ -1,5 +1,4 @@
-import Routes, { DynamicRoute, BasicRoute } from "./Routes"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 import React from "react"
 
 interface BasicRouterProps {
@@ -19,4 +18,55 @@ export default function BasicRouter(props: BasicRouterProps) {
         </div>
         {props.afterRoutes}
     </BrowserRouter>
+}
+export interface BasicRoute {
+    path: string;
+    exact?: boolean;
+    name?: string;
+    child: JSX.Element;
+    icon?: JSX.Element;
+}
+
+export interface DynamicRoute {
+    path: string;
+    exact?: boolean;
+    name?: string;
+    component: any;
+    icon?: JSX.Element;
+}
+
+interface RoutesProps {
+    routes: (DynamicRoute | BasicRoute)[];
+    error404?: BasicRoute;
+    nameToWindowTitle?: boolean;
+}
+
+export function Routes(props: RoutesProps) {
+    const nameForPath = (path: string) => {
+        return props.routes.find((route) => route.path === path)?.name;
+    }
+    if (props.nameToWindowTitle) {
+        const name = nameForPath(window.location.pathname);
+        window.document.title = "Brainstorm" + (name !== undefined ? " - " + name : "");
+    }
+    let displayedRoutes: JSX.Element[] = [];
+    props.routes.forEach(route => {
+        if ((route as BasicRoute).child) {
+            displayedRoutes.push(<Route exact={route.exact} key={route.path} path={route.path} children={(route as BasicRoute).child} />);
+        } else if ((route as DynamicRoute).component) {
+            displayedRoutes.push(<Route exact={route.exact} key={route.path} path={route.path} component={(route as DynamicRoute).component} />);
+        } else {
+            console.error("No Children or Component found.");
+        }
+    })
+
+    if (props.error404) {
+        displayedRoutes.push(<Route exact={props.error404.exact} key={props.error404.path} path="" children={props.error404.child} />)
+    }
+
+    return <Switch>
+        {displayedRoutes}
+    </Switch>
+
+
 }
